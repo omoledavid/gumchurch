0c0029b05400dcc62dd547933a0b86e1f59df6bb
Omole kessiena David <edokadavid@gmail.com>
2025-05-03 12:18:57 +0100

    Update index pages

--- resources/views/pages/sermons/index.blade.php
+++ resources/views/pages/sermons/index.blade.php
@@ -1,20 +1,29 @@
 <x-app-layout :$pageName>
-    <x-banner :$pageName/>
+    <x-banner :$pageName />
     <div class="event-section-2 section-padding pb-80px">
         <div class="container">
-            <div class="row">
-                @foreach($sermons as $sermon)
-                    <x-sermons.sermon-card :$sermon/>
-                @endforeach
-            </div>
-
-
-            @if($sermons->count() > 5)
+            @if ($sermons && $sermons->count() > 0)
                 <div class="row">
-                    <div class="col-12 d-flex justify-content-lg-start justify-content-center mt-5">
-                        <nav aria-label="Page navigation example">
-                            {{ $sermons->links('vendor.pagination.custom') }}
-                        </nav>
+                    @foreach ($sermons as $sermon)
+                        <x-sermons.sermon-card :$sermon />
+                    @endforeach
+                </div>
+
+
+                @if ($sermons->count() > 5)
+                    <div class="row">
+                        <div class="col-12 d-flex justify-content-lg-start justify-content-center mt-5">
+                            <nav aria-label="Page navigation example">
+                                {{ $sermons->links('vendor.pagination.custom') }}
+                            </nav>
+                        </div>
+                    </div>
+                @endif
+            @else
+                <div class="col-12">
+                    <div class="alert alert-info">
+                        <i class="bi bi-info-circle me-2"></i>
+                        No Sermons. Check back later for updates.
                     </div>
                 </div>
             @endif
